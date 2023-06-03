2f4e0d984b0bde233417485cdd7fbee7d1eb51f9
masudrana25 <mdmasudrana2512000@gmail.com>
2023-06-04 03:25:35 +0600

    dfdjkd

--- src/component/Shipment/Shipment.js
+++ src/component/Shipment/Shipment.js
@@ -8,20 +8,23 @@ const Shipment = () => {
   return (
     <form className='ship-form' onSubmit={handleSubmit(onSubmit)}>
   
-      <input className='aa' {...register("name", { required: true })}  placeholder='Your Name'/>
-      {errors.name && <span className='error'>Name is required</span>} <br /> <br />
+      <input className='aa' {...register("name", { required: true })}  placeholder='Your Name'/> <br />
+      {errors.name && <span style={{color:'red'}}>Name is required</span>}  <br />
       
 
-      <input {...register("email", { required: true })}  placeholder='Your email'/>
-      {errors.email && <span className='error'>email is required</span>} <br /> <br />
+      <input {...register("email", { required: true })} placeholder='Your email' />
+      <br />
+      {errors.email && <span style={{color:'red'}}>email is required</span>}  <br />
       
 
-      <input {...register("adress", { required: true })}  placeholder='Your adress'/>
-      {errors.adress && <span className='error'>adress is required</span>} <br /> <br />
+      <input {...register("adress", { required: true })} placeholder='Your adress' />
+      <br />
+      {errors.adress && <span style={{color:'red'}}>adress is required</span>}  <br />
       
 
-      <input {...register("mobile", { required: true })}  placeholder='Your mobile number'/>
-      {errors.mobile && <span className='error'>mobile number is required</span>} <br /> <br />
+      <input {...register("mobile", { required: true })} placeholder='Your mobile number' />
+      <br />
+      {errors.mobile && <span style={{color:'red'}}>mobile number is required</span>} <br />
       <input type="submit" />
     </form>
   );
